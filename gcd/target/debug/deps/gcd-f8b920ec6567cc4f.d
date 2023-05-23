d99dada7d3703ee8a80e20a0ba2e5c7bdaf5cad0
WinstonInRust <rustprogrammeren@gmail.com>
2023-05-23 10:29:16 +0200

    added unit test

---
+++ gcd/target/debug/deps/gcd-f8b920ec6567cc4f.d
@@ -0,0 +1,5 @@
+/Users/winstonhenrimuijs/rust-systems-programming/gcd/target/debug/deps/gcd-f8b920ec6567cc4f: src/main.rs
+
+/Users/winstonhenrimuijs/rust-systems-programming/gcd/target/debug/deps/gcd-f8b920ec6567cc4f.d: src/main.rs
+
+src/main.rs:
